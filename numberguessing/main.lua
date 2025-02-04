e7691bd4f5130134e4c1e28f8d8e5cbf6fdc687e
Mason <mason.esmailbegui@gmail.com>
2025-02-03 21:51:14 -0500

    number guess game

---
+++ numberguessing/main.lua
@@ -0,0 +1,8 @@
+local startingNumber = 1
+local endingNumber = 10
+
+print("Guess a number from " .. startingNumber .. " and " .. endingNumber)
+
+local userNumber = io.read("*n")
+
+print("You selected " .. userNumber)
