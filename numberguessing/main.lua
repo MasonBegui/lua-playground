d262393678878f407a6c0b75a26f0168adf41f0d
Mason <mason.esmailbegui@gmail.com>
2025-02-09 18:37:27 -0500

    Verifying the number is within range

--- numberguessing/main.lua
+++ numberguessing/main.lua
@@ -6,3 +6,7 @@ print("Guess a number from " .. startingNumber .. " and " .. endingNumber)
 local userNumber = io.read("*n")
 
 print("You selected " .. userNumber)
+
+if userNumber < startingNumber or userNumber > endingNumber then
+print("You are wrong.You can only stay withn 1 and 10")
+end
