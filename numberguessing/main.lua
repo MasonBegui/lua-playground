33a1643df56c832fc5cb5a55f63c02de1e062cb9
Mason <mason.esmailbegui@gmail.com>
2025-02-09 20:02:43 -0500

    finished my random number game

--- numberguessing/main.lua
+++ numberguessing/main.lua
@@ -1,12 +1,39 @@
 local startingNumber = 1
 local endingNumber = 10
+local randomNumber = math.random(startingNumber, endingNumber)
 
-print("Guess a number from " .. startingNumber .. " and " .. endingNumber)
+function getusernumber()
+    print("Guess a number from " .. startingNumber .. " and " .. endingNumber)
+    local userNumber = io.read("*n")
 
-local userNumber = io.read("*n")
+    while true do
+        if userNumber >= startingNumber and userNumber <= endingNumber then
+            break;
+        else
+            print("You are wrong.You can only stay withn " .. startingNumber .. " and " .. endingNumber)
+            print("Guess a number from " .. startingNumber .. " and " .. endingNumber)
+            userNumber = io.read("*n")
+        end
+    end
+    return userNumber
+end
+-- Start of program
+
+
+local userNumber = getusernumber()
 
 print("You selected " .. userNumber)
-
-if userNumber < startingNumber or userNumber > endingNumber then
-print("You are wrong.You can only stay withn 1 and 10")
+while true do
+    if userNumber == randomNumber then
+        print("You won")
+        break
+    else
+        --hint
+        if userNumber > randomNumber then
+            print("The Number " .. userNumber .. " is greater than the random number")
+        else
+            print("The number " .. userNumber .. " is less than the random number")
+        end
+        userNumber = getusernumber()
+    end
 end
